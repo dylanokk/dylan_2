3c3a3771b8381ee2d5c39e6e5ded24e9e00cdf88
dylanokk <162610017+dylanokk@users.noreply.github.com>
2024-03-07 14:01:14 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,41 @@
+#1 imprimir numeros del 1 al 100
+x=1
+while x<=100:
+     print(x)
+     x=x+1
+#2 imprimir numeros del 50 al 100     
+x=50
+while x<=100:
+     print(x)
+     x=x+1
+
+#3 imprimir numeros del 50 al 100 pero de 2 en 2
+x=50
+while x<=100:
+     print(x)
+     x=x+2
+
+#4 imprimir numeros del -50 al 0
+
+x=-50
+while x<=0:
+     print(x)
+     x=x+1
+     
+ #1 imprimir numeros del 2 al 100 pero de 2 en 2  
+
+
+x=2
+while x<=100:
+    x=x2
+    print(x)
+    x=x+2
+
+
+n=int (input("ingrese el valor final"))
+x=1
+while x<=n:
+    ptint(x)
+    x=x+1
+     
+     
